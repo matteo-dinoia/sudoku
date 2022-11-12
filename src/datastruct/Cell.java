b363247863c464cf7c68a4fdf0c4a4b995242f7a
Matteo <matteodinoia02@gmail.com>
2022-11-12 01:01:26 +0100

    finally should solve anything

--- src/datastruct/Cell.java
+++ src/datastruct/Cell.java
@@ -5,9 +5,22 @@ import java.util.Arrays;
 public class Cell {
 	private int value, x, y;
 	private boolean possibility[];
+	private Cell overlayCell=null;
 
+	private Cell(Cell c){ //clone withot overlay
+		this.x=c.x;
+		this.y=c.y;
+		this.value=c.value;
+
+		if(c.value>0)
+			return; //don't copy possibility
+
+		this.possibility=new boolean[c.possibility.length];
+		for(int i=0; i<possibility.length; i++)
+			this.possibility[i]=c.possibility[i];
+
+	}
 	public Cell(int x, int y){ this(x, y, 0);}
-
 	public Cell(int x, int y, int num){
 		if(num<1 || num>9){
 			this.value=0;
@@ -21,6 +34,9 @@ public class Cell {
 	}
 
 	public boolean checkUnicityAndSetValue(){
+		if(overlayCell!=null)
+			return overlayCell.checkUnicityAndSetValue();
+
 		if(possibility==null)
 			return false;
 
@@ -48,6 +64,9 @@ public class Cell {
 
 	//SETTER AND GETTER  -------------------------------------------
 	public boolean isPossible(int num){
+		if(overlayCell!=null)
+			return overlayCell.isPossible(num);
+
 		if(num<1 || num>Sudoku.NUM_POSSIBILITY)
 			throw new Error("Error in cell: trying to get possibility of invalid value"+num);
 
@@ -58,10 +77,13 @@ public class Cell {
 	}
 
 	public boolean setPossible(int num, boolean possible){
+		if(overlayCell!=null)
+			return overlayCell.setPossible(num, possible);
+
 		if(num<1 || num>Sudoku.NUM_POSSIBILITY)
 			throw new Error("Error in cell: trying to get possibility of invalid value"+num);
 
-		if(!isValueSet()){
+		if(!hasValue()){
 			if(possibility[num-1]!=possible){
 				possibility[num-1]=possible;
 				return true;
@@ -71,9 +93,12 @@ public class Cell {
 	}
 
 	protected boolean setValue(int num){
+		if(overlayCell!=null)
+			return overlayCell.setValue(num);
+
 		if(num<1 && num>Sudoku.NUM_POSSIBILITY)
 			throw new Error("Error in cell: trying to setting invalid value");
-		if(isValueSet())
+		if(hasValue())
 			return false;
 		value=num;
 		possibility=null;
@@ -81,11 +106,17 @@ public class Cell {
 	}
 
 	public int getValue(){
+		if(overlayCell!=null)
+			return overlayCell.getValue();
+
 		return value;
 	}
 
 	@Override
 	public String toString(){
+		if(overlayCell!=null)
+			return overlayCell.toString();
+
 		if(possibility==null || value>0){
 			return "Cell["+x+","+y+"] -> v="+value+"\n";
 		}
@@ -97,7 +128,14 @@ public class Cell {
 		return res+")\n";
 	}
 
-	private boolean isValueSet(){
+	public boolean hasValue(){
+		if(overlayCell!=null)
+			return overlayCell.hasValue();
+
 		return (value>0 || possibility==null);
+	}
+
+	public void overlayCell(){
+		overlayCell=new Cell(this);
 	}
 }
--- src/datastruct/Sudoku.java
+++ src/datastruct/Sudoku.java
@@ -1,5 +1,6 @@
 package datastruct;
 
+import java.util.*;
 import frontend.FrameIO;
 import utility.Coord;
 import utility.Rect;
@@ -37,8 +38,53 @@ public class Sudoku extends SudokuSets implements Runnable{
 	private Sudoku(Cell[][] allCells) {
 		super(allCells);
 	}
+	private void overlayAll(){
+		for(Cell tmp:cells.values())
+			tmp.overlayCell();
+	}
+
+	private boolean isSolved() {
+		for(Cell tmp:cells.values())
+			if(!tmp.hasValue()) return false;
+		return true;
+	}
 
 	private void startSolver(){
+		oneIterationSolver();
+		Iterator<Cell> cells=this.cells.values().iterator();
+		Cell actualCell=null;
+		int possibility=Sudoku.NUM_POSSIBILITY;
+		while(!isSolved()){
+			debug();
+
+
+			//CHOICE
+			do{
+				if(++possibility<=Sudoku.NUM_POSSIBILITY)
+					continue; //if not change cell
+
+				possibility=1;
+				do{
+					actualCell=cells.next();
+					if(actualCell==null)
+						return;
+				}while(actualCell.hasValue());
+			}while(actualCell.isPossible(possibility));
+
+			//SET
+			overlayAll();
+			actualCell.setValue(possibility);
+
+			try{//ignore error
+				oneIterationSolver();
+			}catch(Error e){}
+
+		}
+	}
+
+
+
+	private void oneIterationSolver(){
 		boolean somethingChanged=true;
 		int counter=0; //TODO remove
 		while(somethingChanged && counter<=10000){ //TODO CHANGE CONDITION TO &&
@@ -57,8 +103,6 @@ public class Sudoku extends SudokuSets implements Runnable{
 			for(SyncedGroup tmp:groupsSynced)
 				somethingChanged=tmp.checkSyncAndSetValue()||somethingChanged;
 		}
-
-		System.out.println(counter);
 	}
 
 	@Override
@@ -73,17 +117,24 @@ public class Sudoku extends SudokuSets implements Runnable{
 	}
 
 	public void debug(){
-		//PRINT GROUP AND SYNC
-		for(CellsGroup tmp: cellsGroup)
-			System.out.println(""+tmp);
-		for(SyncedGroup tmp: groupsSynced)
-			System.out.println(""+tmp);
+		System.out.println("LOL");
 
 		//CREATE MAP
 		Cell[][] allCells=new Cell[9][9];
 		for(int x=0; x<9; x++)
 			for(int y=0; y<9; y++)
 				allCells[x][y]=cells.get(new Coord(x,y));
+
+		f.setMap(allCells);
+
+		/*
+		//PRINT GROUP AND SYNC
+		for(CellsGroup tmp: cellsGroup)
+			System.out.println(""+tmp);
+		for(SyncedGroup tmp: groupsSynced)
+			System.out.println(""+tmp);
+
+
 
 		//PRINT MAP
 		for(int y=0; y<9; y++){
@@ -100,8 +151,6 @@ public class Sudoku extends SudokuSets implements Runnable{
 			if(y!=8)System.out.print(
 				"\n--------------------------------------------------------------------------------------------");
 			System.out.println();
-		}
-
-		f.setMap(allCells);
+		}*/
 	}
 }
